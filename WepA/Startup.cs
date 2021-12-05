28033f7361fed35ef6dc5e9fc37e42f9094eb437
Henry-idiotium <quocdat.381872@gmail.com>
2021-12-05 10:57:03 +0700

    style(startup.cs): Remove rredundant comments

--- WepA/Startup.cs
+++ WepA/Startup.cs
@@ -3,13 +3,10 @@ using WepA.Interfaces.Services;
 using WepA.Extensions.Helpers;
 using WepA.Extensions;
 using WepA.Data;
-using Microsoft.OpenApi.Models;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Autofac;
@@ -32,7 +29,6 @@ namespace WepA
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			// Configure DbContext
 			services.AddDbContext<WepADbContext>(options =>
 				options.UseSqlServer(Configuration.GetConnectionString("WepA")));
 
@@ -43,13 +39,6 @@ namespace WepA
 
 			services.CorsConfiguration();
 
-				// User settings.
-				// options.User.AllowedUserNameCharacters =
-				// "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
-				options.User.RequireUniqueEmail = true;
-			});
-
-			// AutoMapper Service
 			services.AddAutoMapper(typeof(Startup));
 
 			// Provide specified key for JwtUtil class instance - Options pattern
@@ -63,11 +52,7 @@ namespace WepA
 		// here will override any registrations made in ConfigureServices.
 		public void ConfigureContainer(ContainerBuilder builder)
 		{
-			// User manage register
 			builder.RegisterType<UserService>().As<IUserService>();
-			builder.RegisterType<UserRepository>().As<IUserRepository>();
-
-			// Account manage
 			builder.RegisterType<AccountService>().As<IAccountService>();
 		}
 
@@ -81,9 +66,7 @@ namespace WepA
 				app.UseSwagger();
 				app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WepA v1"));
 			}
-
 			app.UseHttpsRedirection();
-
 			app.UseRouting();
 
 			app.UseCors("DevelopmentPolicy");
