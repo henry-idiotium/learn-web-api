82a34899fd9897c62b9d9d3fa9c123f2d8bbfbc0
Henry-idiotium <quocdat.381872@gmail.com>
2021-12-21 14:56:56 +0700

    Remove Autofac

--- WepA/Program.cs
+++ WepA/Program.cs
@@ -1,5 +1,3 @@
-using System.IO;
-using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
@@ -9,24 +7,8 @@ namespace WepA
 	{
 		public static void Main(string[] args)
 		{
-			// CreateHostBuilder(args).Build().Run();
-
-			// ASP.NET Core 3.0+:
-			// The UseServiceProviderFactory call attaches the
-			// Autofac provider to the generic hosting mechanism.
-			var host = Host.CreateDefaultBuilder(args)
-				.UseServiceProviderFactory(new AutofacServiceProviderFactory())
-				.ConfigureWebHostDefaults(webHostBuilder =>
-				{
-					webHostBuilder
-						.UseContentRoot(Directory.GetCurrentDirectory())
-						.UseIISIntegration()
-						.UseStartup<Startup>();
-				}).Build();
-
-			host.Run();
+			CreateHostBuilder(args).Build().Run();
 		}
-
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
 			Host.CreateDefaultBuilder(args)
 				.ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
