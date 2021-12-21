bdc71f39906a952541685b88d7362d0e269110eb
Henry-idiotium <quocdat.381872@gmail.com>
2021-12-21 14:55:54 +0700

    fix(Autofac): :bug: Remove autofac
    Autofac cause common Dependency Injection error.

--- WepA/Startup.cs
+++ WepA/Startup.cs
@@ -1,17 +1,17 @@
-using WepA.Services;
-using WepA.Interfaces.Services;
-using WepA.Extensions.Helpers;
-using WepA.Extensions;
+using WepA.Middlewares;
+using WepA.Helpers.Settings;
+using WepA.Helpers;
 using WepA.Data;
+using System.Net.Mail;
+using System.IO;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
-using Autofac;
-using WepA.Utils;
-using WepA.Interfaces.Utils;
+using System.IdentityModel.Tokens.Jwt;
+using System.Collections.Generic;
 
 namespace WepA
 {
@@ -29,19 +29,15 @@ namespace WepA
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddDbContext<WepADbContext>(options =>
-				options.UseSqlServer(Configuration.GetConnectionString("WepA")));
-
+			JwtSecurityTokenHandler.DefaultInboundClaimTypeMap = new Dictionary<string, string>();
+			services.AddDbContext<WepADbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("WepA")));
+			// service extensions
+			// services.AuthenticationConfiguration(Configuration.GetValue<string>("JwtSettings:Secret"));
 			services.IdentityConfiguration(CurrentEnvironment.IsDevelopment());
-
-			services.AuthenticationConfiguration(
-				Configuration.GetSection("JwtSettings").GetSection("Secret").Value);
-
-			services.CorsConfiguration();
-
 			services.AddAutoMapper(typeof(Startup));
-
-			// Provide specified key for JwtUtil class instance - Options pattern
+			services.CorsConfiguration();
+			services.RegisterDIConfiguration();
+			// option-patterns
 			services.Configure<JwtSettings>(Configuration.GetSection("JwtSettings"));
 			services.Configure<EmailSettings>(Configuration.GetSection("EmailSettings"));
 
@@ -59,17 +55,8 @@ namespace WepA
 			services.SwaggerConfiguration();
 		}
 
-		// This method runs after ConfigureServices, so the methods
-		// here will override any registrations made in ConfigureServices.
-		public void ConfigureContainer(ContainerBuilder builder)
-		{
-			builder.RegisterType<UserService>().As<IUserService>();
-			builder.RegisterType<AccountService>().As<IAccountService>();
-			builder.RegisterType<JwtUtil>().As<IJwtUtil>();
-		}
-
-		// This method gets called by the runtime. Use this method to configure the HTTP request
-		// pipeline.
+		// Middlewares — This method gets called by the runtime. Use this method to configure the
+		// HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
 			if (env.IsDevelopment())
@@ -86,6 +73,7 @@ namespace WepA
 			app.UseAuthentication();
 			app.UseAuthorization();
 
+			app.UseHttpStatusExceptionHandling();
 			app.UseJwtMiddleware();
 
 			app.UseEndpoints(endpoints => endpoints.MapControllers());
